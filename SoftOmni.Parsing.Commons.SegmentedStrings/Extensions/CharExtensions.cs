0a5cf17912c87d0f90d54714faeb0fa3595ea90c
jaschamerle <jascha.merle@gmail.com>
2025-04-19 21:02:01 +0200

    fix(source/extensions): adjusted the namespace and location of the CharExtensions.cs file.

---
+++ SoftOmni.Parsing.Commons.SegmentedStrings/Extensions/CharExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftOmni.Parsing.Commons.SegmentedStrings.Extensions;
+
+public static class CharExtensions
+{
+    public static SegmentedString ToSegmentedString(this char c)
+    {
+        return new SegmentedString(c.ToString());
+    }
+}
