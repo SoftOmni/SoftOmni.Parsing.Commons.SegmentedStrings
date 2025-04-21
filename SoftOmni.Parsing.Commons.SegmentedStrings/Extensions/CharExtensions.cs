3b5bbca5e4f53dae5206023f8f6587d951c331b2
jaschamerle <jascha.merle@gmail.com>
2025-04-21 15:16:53 +0200

    chore(src/extensions/CharExtensions): renamed the parameter c in the extension method of chars ToSegmentedString in the CharExtenions static class to character for readability. Similarly renamed the parameter s in the extension method to strings ToSegmentedString in the CharExtensions static class to @string for readability

--- SoftOmni.Parsing.Commons.SegmentedStrings/Extensions/CharExtensions.cs
+++ SoftOmni.Parsing.Commons.SegmentedStrings/Extensions/CharExtensions.cs
@@ -3,7 +3,13 @@
 public static class CharExtensions
 {
     public static SegmentedString ToSegmentedString(this char c)
+    public static SegmentedString ToSegmentedString(this char character)
     {
         return new SegmentedString(c.ToString());
+        return new SegmentedString(character.ToString());
+    }
+    public static SegmentedString ToSegmentedString(this string @string)
+    {
+        return new SegmentedString(@string);
     }
 }
